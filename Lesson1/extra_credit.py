c9d3fe173ce73fc679ee5b5dbdd3cfc15d6a6a6f
Adam Collins <adc82@case.edu>
2017-06-19 14:33:46 -0500

    extra credit challeng

---
+++ Lesson1/extra_credit.py
@@ -0,0 +1,42 @@
+def find_average(grades):
+    """
+    Given a list of grades find and return the average grade
+    """
+    pass
+
+
+def find_freddy(grades):
+    """
+    You have a student name Freddy, he typically gets the lowest score in the
+    class. Given a list of grades find and return Fredies grade.
+    """
+    pass
+
+
+def find_jordan(grades):
+    """
+    Jordan studies super hard an typically gets the highest grade. In the class
+    find and return Jordan's grade
+    """
+    pass
+
+
+def find_phil(grades):
+    """
+    Phil doesn't like to study, but he's no dummy. He typically get's the
+    lowest A. In this class any student who scores above the average gets an A.
+    Find and return Phil's grade
+    """
+    pass
+
+grades = [85, 88, 80, 92, 100, 99, 82, 84, 70, 69, 72, 91, 80, 75]
+
+average = find_average(grades)
+freddy = find_freddy(grades)
+jordan = find_jordan(grades)
+phil = find_phil(grades)
+
+print("The average is: " + average)
+print("The freddy is: " + freddy)
+print("The jordan is: " + jordan)
+print("The phil got: " + phil)
